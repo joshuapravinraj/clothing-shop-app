35bdb978763105311ea2d30d81609a891b2674f5
Joshua Pravin Raj <orifielgamer@gmail.com>
2020-05-24 23:21:53 +0800

    updating app with shoppage, collection preview component, collection item component

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
