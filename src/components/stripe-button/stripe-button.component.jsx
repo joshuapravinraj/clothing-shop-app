c9d272b9c66da5c821685d722bef6a8f167c9dcd
JPR <origamer@JPR.local>
2021-09-26 16:17:53 +0800

    updated stripe payment integration + updated fav icon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51HzOoVKixQBL1iGal7mW320sgO321uzz9VDNhpXOemtf8a0kHoJ3toS5vEJWCv22vy2gRiBQihmLksBpnBLY3cYx009KWoCMBm';
+
+    const onToken = token => {
+        console.log(token)
+        alert('Payment Successful')
+    }
+    
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='Crown Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
