b880db327bf8b19655540c7ab6b3f957dc24b85d
Joshua Raj <joshua.raj@kodebaze.com>
2022-03-08 23:42:59 +0800

    added container pattern + advance redux thunk + updated styles

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { CustomButtonContainer } from './custom-button.styles';
 
-const CustomButton = ({ children, ...props }) => (
-    <CustomButtonContainer {...props}>
-        {children}
-    </CustomButtonContainer>
+import './custom-button.styles.scss';
+
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+  <button
+    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
 );
 
 export default CustomButton;
