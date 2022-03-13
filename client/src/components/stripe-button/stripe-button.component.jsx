08a76aea4f29c85ec3faff4fe44978e9d6f04b6d
Joshua Raj <joshua.raj@kodebaze.com>
2022-03-13 20:39:42 +0800

    stripe updates

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51HzOoVKixQBL1iGal7mW320sgO321uzz9VDNhpXOemtf8a0kHoJ3toS5vEJWCv22vy2gRiBQihmLksBpnBLY3cYx009KWoCMBm';
 
-    const onToken = () => { alert('Payment Successful') }
+    const onToken = token => { 
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(res => {
+            alert('Payment successful!')
+        }).catch(err => {
+            console.log('Payment failed: ', JSON.parse(err))
+            alert('There was an issue with your payment!')
+        })
+    }
     
     return (
         <StripeCheckout 
