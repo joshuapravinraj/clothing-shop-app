d32d870f04a21243a0950a015a149ccef103b430
Joshua Raj <joshuapravin21@gmail.com>
2022-08-28 12:31:46 -0700

    yarn updated + refactor

--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -8,7 +8,7 @@ const MenuItem = ({ title, imageUrl, size, linkUrl, match }) => {
     return (
         <div 
             className={`${size} menu-item`} 
-            onClick={() => history.push(`${match.url}${linkUrl}`)}
+            onClick={() => history.push(`${match?.url}${linkUrl}`)}
         >
             <div 
                 className='background-image' 
